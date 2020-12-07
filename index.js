cd629d0cd9b30be110d227c9cd0f20501e9bb508
JH <magatriod7@naver.com>
2020-12-08 04:21:40 +0900

    gitsave3 nodemon babel

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleListening() {
- console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleHome = (req, res) => res.send("Hello from my ass");
 
-function handleHome(req, res) {
-  res.send("Hello from home");
-}
-
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening); 
+app.listen(PORT, handleListening);
